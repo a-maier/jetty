e3916ace528ec94f10776ac16968de53da2cb568
Andreas Maier <andreas.martin.maier@desy.de>
2021-02-12 10:01:54 +0100

    added debugging output and support for acceptance cuts

--- src/cluster.rs
+++ src/cluster.rs
@@ -1,8 +1,34 @@
 use crate::pseudojet::PseudoJet;
 use crate::distance::Distance;
 
-// Cluster `partons` into jets using the distance measure `d`
-pub fn cluster<D: Distance>(mut partons: Vec<PseudoJet>, d: &D) -> Vec<PseudoJet> {
+use std::cmp::Ord;
+
+use log::{debug, trace};
+
+fn minmax<T: Ord>(i: T, j: T) -> (T, T) {
+    if i > j {
+        (j, i)
+    } else {
+        (i, j)
+    }
+}
+
+/// Cluster `partons` into jets using the distance measure `d`
+pub fn cluster<D: Distance>(partons: Vec<PseudoJet>, d: &D) -> Vec<PseudoJet> {
+    cluster_if(partons, d, |_| true)
+}
+
+/// Cluster `partons` into jets using the distance measure `d`
+/// Only jets for which `accept` is true are returned
+pub fn cluster_if<D, F>(
+    mut partons: Vec<PseudoJet>,
+    d: &D,
+    mut accept: F
+) -> Vec<PseudoJet>
+where
+    D: Distance,
+    F: FnMut(PseudoJet) -> bool
+{
     let n = partons.len();
 
     let mut dists = Vec::with_capacity((n * (n + 1))/2);
@@ -18,6 +44,7 @@ pub fn cluster<D: Distance>(mut partons: Vec<PseudoJet>, d: &D) -> Vec<PseudoJet
             i, i
         ))
     }
+    trace!("distances: {:#?}", dists);
 
     // TODO: test keeping distances sorted
 
@@ -27,39 +54,44 @@ pub fn cluster<D: Distance>(mut partons: Vec<PseudoJet>, d: &D) -> Vec<PseudoJet
             // closest is beam axis, this is already a jet
             dists.retain(|(_, ii, jj)| *ii != i && *jj != i);
             let jet = partons.swap_remove(i);
+            debug!("new jet: {:?}", jet);
             for (_dist, ii, jj) in &mut dists {
                 if *ii == partons.len() {
-                    *ii = i;
+                    *ii = i
                 }
                 if *jj == partons.len() {
                     *jj = i;
                 }
             }
-            jets.push(jet)
+            trace!("distances: {:#?}", dists);
+            if accept(jet) { jets.push(jet) }
         } else {
             // combine into new pseudojet
+            let (i, j) = minmax(i, j);
             debug_assert!(j > i);
             dists.retain(|(_, ii, jj)| *ii != i && *jj != i && *ii != j && *jj != j);
-            let p1 = partons.swap_remove(i);
             let p2 = partons.swap_remove(j);
+            let p1 = partons.swap_remove(i);
             for (_dist, ii, jj) in &mut dists {
-                if *ii == partons.len() + 1 {
+                if *ii == partons.len() {
                     *ii = i;
-                } else if *ii == partons.len() {
+                } else if *ii == partons.len() + 1 {
                     *ii = j;
                 }
-                if *jj == partons.len() + 1 {
+                if *jj == partons.len() {
                     *jj = i;
-                } else if *jj == partons.len() {
+                } else if *jj == partons.len() + 1 {
                     *jj = j;
                 }
             }
             let pj = p1 + p2;
+            debug!("cluster pseudojets {} {}", i, j);
             let j = partons.len();
             for (i, pi) in partons.iter().copied().enumerate() {
                 dists.push((d.distance(&pi, &pj), i, j));
             }
             dists.push((d.beam_distance(&pj), j, j));
+            trace!("distances: {:#?}", dists);
             partons.push(pj);
         }
     }
