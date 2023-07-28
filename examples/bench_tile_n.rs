94bf9d6d87ab024a31cb6a7ec6b3fe8001b913e0
Andreas Maier <andreas.martin.maier@desy.de>
2023-07-28 11:08:52 +0200

    added benchmarks for geometric tiling clustering

---
+++ examples/bench_tile_n.rs
@@ -0,0 +1,34 @@
+use std::{fs::File, time::Duration};
+
+use anyhow::Result;
+use cpu_time::ProcessTime;
+use jetty::{PseudoJet, anti_kt_f, ClusterStep, cluster::geom_tile::ClusterGeomTile};
+
+fn main() -> Result<()> {
+    let n = std::env::args().skip(1).next().unwrap().parse()?;
+    let input = File::open("data/momenta_showered.rmp.zst")?;
+    let mut events = Vec::new();
+    zstd::stream::copy_decode(input, &mut events).unwrap();
+    let events: Vec<Vec<[f64; 4]>> = rmp_serde::from_slice(&events)?;
+
+    let events: Vec<Vec<PseudoJet>> = events.into_iter().map(
+        |ev| ev.into_iter().take(n).map(|p| p.into()).collect()
+    ).collect();
+    pub const NEVENTS: usize = 10000;
+    assert_eq!(NEVENTS, events.len()); // helps with optimisations
+
+    let start = ProcessTime::now();
+    let njets: usize = events.into_iter()
+        .map(|ev| {
+            let cluster = ClusterGeomTile::new(ev, anti_kt_f(0.4));
+            cluster.filter(|s| match s {
+                ClusterStep::Jet(j) => j.pt2() > 100.,
+                ClusterStep::Combine(_) => false,
+            }).count()
+        }).sum();
+    let cpu_time: Duration = start.elapsed();
+    let avg_njets = njets as f64 / NEVENTS as f64;
+    println!("Found {avg_njets:1} jets per event in {cpu_time:?}");
+
+    Ok(())
+}
